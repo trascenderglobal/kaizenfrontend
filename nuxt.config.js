920f1d00e82d213fe73b107f448a294c8fe3675d
Sebastian Rendon <7554810-serendongi@users.noreply.gitlab.com>
2021-06-25 12:33:52 -0500

    fix: rename Vuelidate to vuelidate

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,7 +27,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { src: '~/plugins/Vuelidate' },
+    { src: '~/plugins/vuelidate' },
     { src: '~/plugins/datepicker', mode: 'client' },
   ],
 
