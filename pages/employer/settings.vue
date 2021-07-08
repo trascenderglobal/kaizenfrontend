d60ceaaab49369867634ef71c7f1728a299cea77
jsapuyesp <jsapuyesp@unal.edu.co>
2021-07-08 18:36:30 -0500

    feat(settings): delete account | select language

--- pages/employer/settings.vue
+++ pages/employer/settings.vue
@@ -5,38 +5,162 @@
         {{ $t('settings.settings') }}
       </h1>
     </div>
-    <h1 class="pt-6 text-lg text-blue-kaizen">
+    <h1 class="title">
       {{ $t('settings.header') }}
     </h1>
-    <div class="flex justify-between pt-6">
-      <div class="flex space-x-4">
-        <div class="user-img-lg"></div>
+    <div class="flex flex-wrap justify-between pt-6">
+      <div class="flex flex-grow lg:flex-grow-0 space-x-4">
+        <div class="user-img-lg">
+          <div class="img-wrapper">
+            <iconly-icon
+              name="camera"
+              :size="1.2"
+              class="fill-current text-white"
+            />
+            <div
+              role="img"
+              :aria-label="$t('profile.userImage')"
+              class="img"
+              :style="userImage"
+            ></div>
+          </div>
+        </div>
         <div class="flex flex-col space-y-2">
           <span class="font-medium text-blue-kaizen">{{
-            profile.name + ' ' + profile.lastName
+            $t('settings.account')
           }}</span>
           <span class="font-light text-gray-dark">{{
             $t('settings.accountCreatedOn')
           }}</span>
           <span class="font-light text-gray-dark">{{
-            profile.created_at
-              ? $d(new Date(), 'numeric')
-              : $t('settings.noDate')
+            profile.created_at ? $d(profile.created_at) : $t('settings.noDate')
           }}</span>
         </div>
       </div>
-      <div class="flex flex-col space-y-2 min-w-40">
+      <div class="flex flex-col space-y-2 min-w-full lg:min-w-40">
         <span class="font-medium text-blue-kaizen">{{
           $t('settings.language')
         }}</span>
-        <span class="font-light text-gray-dark">{{
-          $t('settings.language')
-        }}</span>
+        <ks-language-list />
       </div>
     </div>
     <hr class="my-8 border" />
-
-    <hr class="my-8 border" />
+    <div class="flex flex-wrap overflow-hidden">
+      <div class="w-1/2 overflow-hidden sm:w-full md:w-full lg:w-1/2 xl:w-1/2">
+        <h1 class="title">{{ $t('settings.deleteAccount') }}</h1>
+        <div class="flex justify-start py-2">
+          <ks-btn large font="font-normal" @click="showDelete = true">{{
+            $t('settings.deleteMyAccount')}}
+          </ks-btn>
+        </div>
+        <span class="py-2 text-gray-darker font-light">{{
+          $t('settings.deleteLeaveSure')}}
+        </span>
+        <transition name="edit">
+          <div v-if="showDelete" class="saved-modal">
+            <div class="w-3/5">
+              <ks-card class="p-8" col>
+                <div class="flex flex-col pt-16 flex-grow items-center space-y-8">
+                  <h1 class="text-3xl font-medium text-center text-blue-kaizen">
+                    {{ $t('settings.deleteMyAccount') }}
+                  </h1>
+                  <div class="user-img-lg">
+                    <div class="img-wrapper">
+                      <iconly-icon
+                        name="camera"
+                        :size="1.2"
+                        class="fill-current text-white"
+                      />
+                      <div class="img" :style="userImage"></div>
+                    </div>
+                  </div>
+                  <hr class="self-stretch" />
+                  <p class="text-blue-kaizen text-center">
+                    {{ $t('settings.deleteLeave') }}
+                  </p>
+                  <p class="text-2xl font-medium text-blue-kaizen text-center">
+                    {{ $t('settings.deleteSure') }}
+                  </p>
+                  <div class="flex space-x-6 pt-8 flex-grow items-end justify-center">
+                    <ks-btn
+                      color="transparent"
+                      outline
+                      class="text-xl"
+                      @click="showDelete = false"
+                      >{{ $t('settings.cancel') }}</ks-btn
+                    >
+                    <ks-btn
+                      color="danger"
+                      class="text-xl"
+                      :loading="deleting"
+                      @click="deleteProfile"
+                      >{{ $t('settings.delete') }}</ks-btn
+                    >
+                  </div>
+                </div>
+              </ks-card>
+            </div>
+          </div>
+        </transition>
+      </div>
+      <div class="w-1/2 overflow-hidden sm:w-full md:w-full lg:w-1/2 xl:w-1/2">
+        <h1 class="title">{{ $t('settings.deleteAccount') }}</h1>
+        <div class="flex justify-start py-2">
+          <ks-btn large font="font-normal" @click="showDelete = true">{{
+            $t('settings.deleteMyAccount')}}
+          </ks-btn>
+        </div>
+        <span class="py-2 text-gray-darker font-light">{{
+          $t('settings.deleteLeaveSure')}}
+        </span>
+        <transition name="edit">
+          <div v-if="showDelete" class="saved-modal">
+            <div class="w-3/5">
+              <ks-card class="p-8" col>
+                <div class="flex flex-col pt-16 flex-grow items-center space-y-8">
+                  <h1 class="text-3xl font-medium text-center text-blue-kaizen">
+                    {{ $t('settings.deleteMyAccount') }}
+                  </h1>
+                  <div class="user-img-lg">
+                    <div class="img-wrapper">
+                      <iconly-icon
+                        name="camera"
+                        :size="1.2"
+                        class="fill-current text-white"
+                      />
+                      <div class="img" :style="userImage"></div>
+                    </div>
+                  </div>
+                  <hr class="self-stretch" />
+                  <p class="text-blue-kaizen text-center">
+                    {{ $t('settings.deleteLeave') }}
+                  </p>
+                  <p class="text-2xl font-medium text-blue-kaizen text-center">
+                    {{ $t('settings.deleteSure') }}
+                  </p>
+                  <div class="flex space-x-6 pt-8 flex-grow items-end justify-center">
+                    <ks-btn
+                      color="transparent"
+                      outline
+                      class="text-xl"
+                      @click="showDelete = false"
+                      >{{ $t('settings.cancel') }}</ks-btn
+                    >
+                    <ks-btn
+                      color="danger"
+                      class="text-xl"
+                      :loading="deleting"
+                      @click="deleteProfile"
+                      >{{ $t('settings.delete') }}</ks-btn
+                    >
+                  </div>
+                </div>
+              </ks-card>
+            </div>
+          </div>
+        </transition>
+      </div>
+    </div>
   </ks-card>
 </template>
 
@@ -46,59 +170,81 @@ import Vue from 'vue'
 type NullableDate = null | Date
 
 export default Vue.extend({
+  name: 'SettingsPage',
   layout: 'employer',
   data() {
     return {
       profile: {
+        created_at: null as NullableDate,
         name: '',
         lastName: '',
-        birthDate: null as NullableDate,
-        contactPerson: '',
-        adress: '',
-        industry: '',
-        position: '',
-        state: '',
-        city: '',
-        phone: '',
         email: '',
-        zip: '',
-        linkedin: '',
         profile_picture_URL: '',
       },
+      showDelete: false,
+      deleting: false,
     }
   },
   async fetch() {
     try {
-      const res = await this.$axios.$get('/employer/profile')
-      res.birthDate = res.birthDate ? new Date(res.birthDate) : null
+      const res = await this.$axios.$get('/employer/settings')
+      res.created_at = res.created_at ? new Date(res.created_at) : null
       this.profile = res
     } catch (error) {}
+  },
+  computed: {
+    userImage(): object {
+      if (this.profile.profile_picture_URL)
+        return {
+          'background-image' : `url(${this.profile.profile_picture_URL})`,
+        }
+      return {}
+    },
+  },
+  methods: {
+    async deleteProfile(): Promise<void> {
+      try {
+        this.deleting = true
+        await this.$axios.$delete('/user/delete')
+        await this.$auth.logout()
+      } catch (error) {
+      } finally {
+        this.deleting = false
+        this.showDelete = false
+      }
+    },
   },
   fetchOnServer: false,
 })
 </script>
 
 <style scoped>
-.settings-header {
-  @apply flex justify-between text-blue-kaizen flex-col lg:flex-row;
+hr {
+  @apply my-6 border-blue-light;
 }
+
+.settings-header {
+  @apply flex justify-start text-blue-kaizen pb-6;
+}
+
 .settings-header > * {
   @apply pt-4;
 }
+
 .user-img-lg {
-  @apply flex-shrink-0 w-20 h-20 rounded-md bg-gray-darker animate-pulse;
+  @apply flex-shrink-0 w-20 h-20;
 }
 
-.social-btn {
-  @apply focus:outline-none rounded-lg;
+.img-wrapper {
+  @apply relative flex items-center justify-center w-full h-full rounded-lg bg-gradient-to-b from-gray-darker to-gray-light shadow-md backdrop-filter backdrop-blur-md;
 }
 
-.edit-profile-btn {
-  @apply inline-flex items-center justify-between w-full space-x-4  focus:outline-none;
+.img-wrapper > .img {
+  @apply w-full h-full bg-no-repeat bg-center z-10 bg-cover absolute rounded-md top-0 left-0 right-0 bottom-0;
 }
 
-.item-value {
-  @apply text-gray-darker font-light py-1;
+.title {
+  @apply text-lg text-blue-kaizen;
 }
 
 .edit-enter-active,
@@ -113,4 +259,5 @@ export default Vue.extend({
 .saved-modal {
   @apply flex items-center justify-center absolute left-0 top-0 w-full h-full bg-gray-lightest z-10 bg-opacity-60 backdrop-filter backdrop-blur;
 }
+
 </style>
